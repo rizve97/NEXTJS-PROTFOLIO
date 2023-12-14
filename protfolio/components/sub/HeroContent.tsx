4a9034a19231035dfa0c214d0e148898c093f902
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-12-14 10:24:46 +0000

    hero section design

--- protfolio/components/sub/HeroContent.tsx
+++ protfolio/components/sub/HeroContent.tsx
@@ -25,7 +25,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            Fullstack Developer
           </h1>
         </motion.div>
 
@@ -34,13 +34,13 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              {" "}
-              the best{" "}
+            RIZVE AHMED
             </span>
-            project exprience
-          </span>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              Full-Stack Web Developer{" "}
+               </span>
+               
+          
         </motion.div>
 
         <motion.p
