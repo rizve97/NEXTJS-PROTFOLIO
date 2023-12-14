8af77f0883cd5dd8a1331f676192be64747be262
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-12-14 10:03:52 +0000

    hero section design

--- protfolio/components/main/Hero.tsx
+++ protfolio/components/main/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeroContent from '../sub/HeroContent';
 
 const Hero = () => {
   return (
@@ -11,7 +12,7 @@ const Hero = () => {
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
-    
+      <HeroContent/>
     </div>
   )
 }
